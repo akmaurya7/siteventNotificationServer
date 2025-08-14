27757dbd49ada68c20b688c21459f04b77b14552
Adarsh Kumar Maurya <amaurya4570@gmail.com>
2025-08-14 11:06:21 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "sitevent"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/SendMessageRoutes.kt
@@ -0,0 +1,35 @@
+import com.example.sitevent.Notification.FirebaseMessaging.SendMessageDto
+import com.example.sitevent.Notification.FirebaseMessaging.toMessage
+import com.google.firebase.messaging.FirebaseMessaging
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.request.receiveNullable
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.post
+import io.ktor.server.routing.route
+
+fun Route.sendNotification(){
+    route("/send"){
+        post {
+            val body = call.receiveNullable<SendMessageDto>()?: kotlin.run {
+                call.respond(HttpStatusCode.BadRequest)
+                return@post
+            }
+
+            FirebaseMessaging.getInstance().send(body.toMessage())
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+
+    route("/broadcast"){
+        post {
+            val body = call.receiveNullable<SendMessageDto>()?: kotlin.run {
+                call.respond(HttpStatusCode.BadRequest)
+                return@post
+            }
+
+            FirebaseMessaging.getInstance().send(body.toMessage())
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+}
